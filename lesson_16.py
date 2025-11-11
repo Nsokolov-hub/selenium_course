174ec003bed488afc2aba7a56fedd6684d10810f
Николай Соколов <bug.hunter.nikolay@mail.ru>
2025-11-11 17:14:11 +0300

    Работа с дропдауном

---
+++ lesson_16.py
@@ -0,0 +1,30 @@
+import time
+
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.select import Select
+
+options = Options()
+options.add_argument("--disable-blink-features=AutomationControlled")
+options.add_argument("--user-agent=Automation QA (Windows NT 6.1; rv:106.0) Gecko/20100101 Firefox/106.0")
+options.page_load_strategy = "normal"
+
+service = Service(ChromeDriverManager().install())
+driver = webdriver.Chrome(service=service, options=options)
+wait = WebDriverWait(driver, 10, poll_frequency=1)
+
+driver.get("http://the-internet.herokuapp.com/dropdown")
+
+select_locator = ("xpath", "//select[@id='dropdown']")
+
+DROPDOWN = Select(driver.find_element(*select_locator))
+time.sleep(3)
+DROPDOWN.select_by_index(2)
+time.sleep(3)
+
+ALL_OPTIONS = DROPDOWN.options
+print(ALL_OPTIONS)
